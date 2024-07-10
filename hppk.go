90b20a1cc75d197574e911c60a48f3e860295738
xtaci <daniel820313@gmail.com>
2024-07-10 19:52:48 +0800

    save

--- hppk.go
+++ hppk.go
@@ -2,6 +2,7 @@ package hppk
 
 import (
 	"crypto/rand"
+	"errors"
 	"math/big"
 )
 
@@ -19,17 +20,25 @@ const (
 type PrivateKey struct {
 	r0, s0 *big.Int // r,s are coprimes
 	r1, s1 *big.Int
+	f0, f1 *big.Int // f(x) = a1x + a0 ,h(x) = b1x+ b0
+	h0, h1 *big.Int
 	PublicKey
 }
 
 // PublicKey represents a public key in the DPPK protocol.
 type PublicKey struct {
-	vecU []*big.Int // Coefficients for polynomial U
-	vecV []*big.Int // Coefficients for polynomial V
+	Prime *big.Int
+	P     []*big.Int
+	Q     []*big.Int
 }
 
 // GenerateKey generates a new DPPK private key with the given order and default prime number
-func GenerateKey() (*PrivateKey, error) {
+func GenerateKey(order int) (*PrivateKey, error) {
+	// Ensure the order is at least 5
+	if order < 5 {
+		return nil, errors.New(ERRMSG_ORDER)
+	}
+
 RETRY:
 	prime, _ := big.NewInt(0).SetString(PRIME, 10)
 	r0, s0, err := createCoPrimePair(prime)
@@ -41,8 +50,68 @@ RETRY:
 		return nil, err
 	}
 
-	if r0.Cmp(r1) == 0 || s0.Cmp(s1) == 0 {
+	f0, err := rand.Int(rand.Reader, prime)
+	if err != nil {
+		return nil, err
+	}
+
+	f1, err := rand.Int(rand.Reader, prime)
+	if err != nil {
+		return nil, err
+	}
+
+	h0, err := rand.Int(rand.Reader, prime)
+	if err != nil {
+		return nil, err
+	}
+
+	h1, err := rand.Int(rand.Reader, prime)
+	if err != nil {
+		return nil, err
+	}
+
+	if r0.Cmp(r1) == 0 || s0.Cmp(s1) == 0 || f0.Cmp(f1) == 0 {
 		goto RETRY
+	}
+
+	// Generate random coefficients for the polynomial Bn(x)
+	Bn := make([]*big.Int, order)
+	for i := 0; i < len(Bn); i++ {
+		r, err := rand.Int(rand.Reader, prime)
+		if err != nil {
+			return nil, err
+		}
+		Bn[i] = r
+	}
+	Bn = append(Bn, big.NewInt(1))
+
+	// generate P, Q
+	P := make([]*big.Int, order+2)
+	Q := make([]*big.Int, order+2)
+
+	// λ + n + 1 == 1 + order + 1 = order + 2
+	for i := 0; i < order+2; i++ {
+		P[i] = big.NewInt(0)
+		Q[i] = big.NewInt(0)
+	}
+
+	bigInt := new(big.Int)
+	// multiply f(x) and h(X) with Bn to get P and Q,
+	// the coefficients of the polynomial Bn has a length of order + 1
+	for i := 0; i < order+1; i++ {
+		// Vector P
+		P[i].Add(P[i], bigInt.Mul(f0, Bn[i]))
+		ring(r0, s0, P[i])
+
+		P[i+1].Add(P[i+1], bigInt.Mul(f1, Bn[i]))
+		ring(r0, s0, P[i+1])
+
+		// Vector Q
+		Q[i].Add(Q[i], bigInt.Mul(h0, Bn[i]))
+		ring(r1, s1, Q[i])
+
+		Q[i+1].Add(Q[i+1], bigInt.Mul(h1, Bn[i]))
+		ring(r1, s1, Q[i+1])
 	}
 
 	return &PrivateKey{
@@ -50,7 +119,108 @@ RETRY:
 		s0: s0,
 		r1: r1,
 		s1: s1,
+		f0: f0,
+		f1: f1,
+		h0: h0,
+		h1: h1,
+		PublicKey: PublicKey{
+			Prime: prime,
+			P:     P,
+			Q:     Q,
+		},
 	}, nil
+}
+
+// ring computes R*v on the ring S
+func ring(R *big.Int, S *big.Int, v *big.Int) {
+	v.Mul(R, v)
+	v.Mod(v, S)
+}
+
+// Encrypt encrypts a message using the given public key.
+func (priv *PrivateKey) Encrypt(pk *PublicKey, msg []byte) (Ps *big.Int, Qs *big.Int, err error) {
+	// Convert the message to a big integer
+	secret := new(big.Int).SetBytes(msg)
+	if secret.Cmp(priv.PublicKey.Prime) >= 0 {
+		return nil, nil, errors.New(ERRMSG_DATA_EXCEEDED_FIELD)
+	}
+
+	// generate a noise
+	/*
+		noise, err := rand.Int(rand.Reader, pk.Prime)
+		if err != nil {
+			return nil, nil, err
+		}
+	*/
+
+	// Initialize variables for the encryption process
+	Si := new(big.Int).Set(secret)
+
+	// Compute the encrypted values Ps and Qs
+	// constants
+	Ps = new(big.Int).Set(pk.P[0])
+	Qs = new(big.Int).Set(pk.Q[0])
+	t := new(big.Int)
+	for i := 1; i < len(pk.P); i++ { // start from non constant item
+		//	SiNoise := new(big.Int).Set(Si)
+		//	SiNoise.Mul(SiNoise, noise)
+		//	SiNoise.Mod(SiNoise, pk.Prime)
+
+		t.Mul(Si, pk.P[i])
+		Ps.Add(Ps, t)
+		Ps.Mod(Ps, pk.Prime)
+
+		t.Mul(Si, pk.Q[i])
+		Qs.Add(Qs, t)
+		Qs.Mod(Qs, pk.Prime)
+
+		Si.Mul(Si, secret)
+		Si.Mod(Si, pk.Prime)
+	}
+
+	return Ps, Qs, nil
+}
+
+// Decrypt decrypts the encrypted values Ps and Qs using the private key.
+func (priv *PrivateKey) Decrypt(P *big.Int, Q *big.Int) (secret *big.Int, err error) {
+	// symmetric encryption
+	revR0 := new(big.Int).ModInverse(priv.r0, priv.s0)
+	revR1 := new(big.Int).ModInverse(priv.r1, priv.s1)
+
+	pbar := new(big.Int).Mul(P, revR0)
+	qbar := new(big.Int).Mul(Q, revR1)
+
+	pbar.Mod(pbar, priv.s0)
+	qbar.Mod(qbar, priv.s1)
+
+	pbar.Mod(pbar, priv.PublicKey.Prime)
+	qbar.Mod(qbar, priv.PublicKey.Prime)
+
+	// noise elimination
+	reqbar := new(big.Int).ModInverse(qbar, priv.PublicKey.Prime)
+	k := new(big.Int).Mul(pbar, reqbar)
+	k.Mod(k, priv.PublicKey.Prime)
+
+	// solve f(x) - k*h(x) =0
+	// f1 * x + f0 - k(h1x+h0) = 0
+	// (f1 - k*h1)x + f0 - kh0 = 0
+	//	x = (kh0 - f0)  / (f1 - k*h1)
+	kh1 := new(big.Int).Mul(k, priv.h1)
+	revkh1 := new(big.Int).Sub(priv.PublicKey.Prime, kh1)
+	denom := new(big.Int).Add(priv.f1, revkh1)
+	denom.Mod(denom, priv.PublicKey.Prime)
+
+	kh0 := new(big.Int).Mul(k, priv.h0)
+	revf0 := new(big.Int).Sub(priv.PublicKey.Prime, priv.f0)
+	numer := new(big.Int).Add(kh0, revf0)
+	numer.Mod(numer, priv.PublicKey.Prime)
+
+	revDenom := new(big.Int).ModInverse(denom, priv.PublicKey.Prime)
+
+	x := new(big.Int).Mul(numer, revDenom)
+	x.Mod(x, priv.PublicKey.Prime)
+	return x, nil
+
 }
 
 func createCoPrimePair(p *big.Int) (R *big.Int, S *big.Int, err error) {
--- hppk_test.go
+++ hppk_test.go
@@ -1,6 +1,7 @@
 package hppk
 
 import (
+	"bytes"
 	"math/big"
 	"testing"
 
@@ -8,7 +9,7 @@ import (
 )
 
 func TestGenerateKey(t *testing.T) {
-	key, err := GenerateKey()
+	key, err := GenerateKey(5)
 	assert.Nil(t, err, "GenerateKey() failed")
 	gcd := new(big.Int)
 	one := big.NewInt(1)
@@ -16,8 +17,27 @@ func TestGenerateKey(t *testing.T) {
 	assert.Equal(t, gcd.GCD(nil, nil, key.r1, key.s1), one, "GCD(r1, s1) != 1")
 }
 
+func TestHPPK(t *testing.T) {
+	alice, err := GenerateKey(10)
+	assert.Nil(t, err)
+	bob, err := GenerateKey(10)
+	assert.Nil(t, err)
+
+	secret := []byte("hello quantum")
+	Ps, Qs, err := bob.Encrypt(&alice.PublicKey, secret)
+	assert.Nil(t, err)
+	t.Log("secret:", string(secret))
+
+	x, err := alice.Decrypt(Ps, Qs)
+	assert.Nil(t, err)
+	t.Log("x:", string(x.Bytes()))
+
+	equal := bytes.Equal(secret, x.Bytes())
+	assert.True(t, equal)
+}
+
 func BenchmarkGenerateKey(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		GenerateKey()
+		GenerateKey(5)
 	}
 }

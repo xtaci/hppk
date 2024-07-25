836959583c79dba3ed88e1dbeec1241934ef39c2
xtaci <daniel820313@gmail.com>
2024-07-25 15:07:15 +0800

    add license to all files

--- hppk.go
+++ hppk.go
@@ -1,13 +1,18 @@
-// Package hppk implements the Hierarchical Polynomial Public Key (HPPK) cryptosystem.
+// # Copyright (c) 2024 xtaci
 //
-// HPPK introduces a novel Homomorphic Polynomial Public Key for
-// Key Encapsulation Mechanism (KEM) and Digital Signatures (DS).
-// By exploiting the inherent partial homomorphic properties of the modular
-// multiplicative permutations, HPPK offers a robust symmetric encryption
-// mechanism for asymmetric cryptography, independent of NP-hard problems. The
-// seamless integration of KEM and DS within HPPK results in compact key sizes,
-// cipher sizes, and signature sizes, demonstrating exceptional performance across
-// various cryptographic operations
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package hppk
 
 import (
---
+++ pkg.go
@@ -0,0 +1,11 @@
+// Package hppk implements the Hierarchical Polynomial Public Key (HPPK) cryptosystem.
+//
+// HPPK introduces a novel Homomorphic Polynomial Public Key for
+// Key Encapsulation Mechanism (KEM) and Digital Signatures (DS).
+// By exploiting the inherent partial homomorphic properties of the modular
+// multiplicative permutations, HPPK offers a robust symmetric encryption
+// mechanism for asymmetric cryptography, independent of NP-hard problems. The
+// seamless integration of KEM and DS within HPPK results in compact key sizes,
+// cipher sizes, and signature sizes, demonstrating exceptional performance across
+// various cryptographic operations
+package hppk
